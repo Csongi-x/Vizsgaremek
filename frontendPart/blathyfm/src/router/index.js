07b8601dc55fb3b9d2dab237812cf84a0075a262
Csongi-x <susancsongor08@gmail.com>
2026-02-23 09:34:46 +0100

    Uploading frontendPart to make exchanging code between members easier

---
+++ frontendPart/blathyfm/src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AdminView from '@/views/adminView/AdminView.vue'
+import SchedulerView from '@/views/schedulerView/SchedulerView.vue'
+import RequestView from "@/views/studentsView/RequestView.vue";
+import SendView from '@/views/studentsView/SendView.vue'
+import LoginView from '@/views/LoginView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+  {
+      path: '/request',
+      name: 'request',
+      component: RequestView,
+  },
+  {
+      path: '/send',
+      name: 'send',
+      component: SendView,
+  },
+  {
+      path: '/scheduler',
+      name: 'scheduler',
+      component: SchedulerView,
+  },
+  {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView,
+  },
+  ],
+})
+
+export default router
---
+++ frontendPart/blathyfm/src/views/LoginView.vue
@@ -0,0 +1,51 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "LoginView"
+})
+</script>
+
+<template>
+<div class="row align-items-center">
+  <div class="col-md-2 col-lg-4"></div>
+  <div class="col-12 col-sm-12 col-md-8 col-lg-4">
+    <div class="loginPage">
+      <form class="w-100 align-items-center">
+        <p class="p-1">
+          <!--Email cim-->
+          <label>E-mail cím: </label>
+          <input name="email" id="email" required>
+          <br>
+          <!--Jelszó-->
+          <label>Jelszó: </label>
+          <input name="password" id="password" required>
+        </p>
+        <!--bejelentkezés-->
+        <button type="submit">Bejelentkezés</button>
+      </form>
+    </div>
+  </div>
+  <div class="col-md-2 col-lg-4"></div>
+</div>
+</template>
+
+<style scoped>
+.loginPage{
+  background-color: gold;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+}
+button {
+  border: 2px solid black;
+  background-color: gold;
+  width: 100%;
+}
+input {
+  margin: .25rem;
+  border: 2px solid black;
+}
+</style>
