4debfb2dc65cf4295fc12e45a584acaaa0319df1
Csongi-x <susancsongor08@gmail.com>
2026-03-23 15:05:54 +0100

    Uploading the repository's content to solve the conflict

---
+++ BlathyFM/blathyfm_frontend/src/router/index.js
@@ -0,0 +1,126 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeLayout from '@/components/layouts/HomeLayout.vue'
+import AdminView from '@/views/adminView/AdminView.vue'
+import SchedulerHomeView from '@/views/schedulerView/SchedulerHomeView.vue'
+import RequestView from "@/views/studentsView/RequestView.vue";
+import SendView from '@/views/studentsView/SendView.vue'
+import LoginView from '@/views/LoginView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { // Főoldal és azok variánsai
+      path: '/',
+      component: HomeLayout,
+      children: [
+          {
+              path: 'home',
+              name: 'main-home',
+              component: () => import('@/views/HomeView.vue'),
+              meta: {
+                  title: "Főoldal",
+                  requiresAuth: false
+              }
+          },
+          {
+              path: 'student/home',
+              name: 'student-home',
+              component: () => import('@/views/studentsView/StudentHomeView.vue'),
+              meta: {
+                  title: "Főoldal",
+                  requiresAuth: true,
+                  roles: ['student']
+              }
+          },
+          {
+              path: 'admin/home',
+              name: 'admin-home',
+              component: () => import('@/views/adminView/AdminHomeView.vue'),
+              meta: {
+                  title: "Főoldal",
+                  requiresAuth: true,
+                  roles: ['admin']
+              }
+          },
+          {
+              path: 'scheduler/home',
+              name: 'scheduler-home',
+              component: () => import('@/views/schedulerView/SchedulerHomeView.vue'),
+              meta: {
+                  title: "Főoldal",
+                  requiresAuth: true,
+                  roles: ['scheduler']
+              }
+          }
+      ]
+    },
+    { // Bejelentkezési képernyő
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+        meta: {
+            title: "Bejelentkezés",
+            requiresAuth: false
+        }
+    },
+  { // Tanulói nézetek (nem ugyanolyan struktúrájúak, éppen ezért nem lesz gyerek komponensezés!)
+      path: '/student/request',
+      name: 'request',
+      component: RequestView,
+      meta: {
+          title: "Kérés",
+          requiresAuth: true,
+          roles: ['student']
+      }
+  },
+  {
+      path: '/student/send',
+      name: 'send',
+      component: SendView,
+      meta: {
+          title: "Küldés",
+          requiresAuth: true,
+          roles: ['student']
+      }
+  },
+  {
+      path: '/scheduler/schedule',
+      name: 'scheduler',
+      component: SchedulerHomeView,
+      meta: {
+          title: "Ütemezés",
+          requiresAuth: true,
+          roles: ['scheduler']
+      }
+  },
+  {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView,
+      meta: {
+          title: "Zenék ellenőrzése",
+          requiresAuth: true,
+          roles: ['admin']
+      }
+  },
+      {
+          path: '/403',
+          name: 'forbidden',
+          component: () => import('@/views/errorsView/ForbiddenView.vue')
+      },
+      {
+          path: '/:pathMatch(.*)*',
+          name: 'not-found',
+          component: () => import('@/views/errorsView/NotFoundView.vue')
+      }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title} - BláthyFM`
+    // autentikáció
+    next()
+})
+
+export default router
