1ffb5015b6d7a988ecfa350b090a29ead8bf0320
BigChongus20 <legendcsongi@gmail.com>
2026-03-24 08:59:50 +0100

    Started working on SchedulerView

--- BlathyFM/blathyfm_frontend/src/views/schedulerView/SchedulerView.vue
+++ BlathyFM/blathyfm_frontend/src/views/schedulerView/SchedulerView.vue
@@ -1,11 +1,56 @@
 <script>
-export default {
+export default{
+  name: "SchedulerView",
+  data(){
+    return{
+      songs:[],
+      playlist:[],//lesz egy gomb a zenék mellett és ha rányom a rendező akkor egy methoddal hozzáadja a playlist-hez
+      requestedSongs:[],
+      loading: false,
+      error: '',
+      }
+    },
+  methods:{
+    async fetchMusic(){
+      this.loading = true
+      this.error = '';
+      try{
+        //BACKEND CONTROLLERBEN /songs AZ ÚTVONAL
+        const response = await http.get('/songs');
+        this.songs = response.data;
+      }catch(error){
+        this.error = error.message;
+      }finally{
+        this.loading = false;
+      }
+    },
+    addMusicToPlaylist(){
 
+    }
+  },
+  mounted(){
+    //itt hívom meg az adatlekérést hogy betöltse a songs tömböt
+    this.fetchMusic();
+  }
 }
 </script>
 
 <template>
+<section>
+  <!--1. oszlop ami minden zene-->
+  <div><!-- itt azt nézze meg hogy be lett e kérve ez a zene-->
 
+  </div>
+  <div><!--itt azt nézi meg hogy benne van e a playlistben-->
+
+  </div>
+  <div><!--itt pedig az összes többi (v-else)-->
+
+  </div>
+  <!--2. oszlop amibe elágazással hogy bekért zene e vagy nem-->
+
+  <!--3. oszlop megint elágazással, amelyik meg be lett rakva a lejátszási listára-->
+</section>
 </template>
 
 <style scoped>
