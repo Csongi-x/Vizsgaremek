7430bedf01bc4e4bf68c5211d44d8687ca0e99be
BigChongus20 <legendcsongi@gmail.com>
2026-04-22 12:20:46 +0200

    Made some changes in the SchedulerView.vue

--- BlathyFM/blathyfm_frontend/src/views/schedulerView/SchedulerView.vue
+++ BlathyFM/blathyfm_frontend/src/views/schedulerView/SchedulerView.vue
@@ -1,156 +1,181 @@
 <script>
 import SchedulerMusicRow from "@/components/music-row/SchedulerMusicRow.vue";
 import RequestView from "@/views/studentsView/RequestView.vue";
-import {http} from '@/utils/http.js'
-import Spinner from "@/components/Spinner.vue";
 import PlaylistMusicRow from "@/components/music-row/PlaylistMusicRow.vue";
-//music, requestedMusic, playlist -» ezek kellenek
-export default{
+import Spinner from "@/components/Spinner.vue";
+import { http } from "@/utils/http.js";
+
+export default {
   name: "SchedulerView",
-  components: {PlaylistMusicRow, Spinner, SchedulerMusicRow, RequestView},
-  data(){
-    return{
-      songs:[],// Minden zene
-      playlistSongs:[],//lesz egy gomb a zenék mellett és ha rányom a rendező akkor egy methoddal hozzáadja a playlist-hez
-      requestedSongs:[],
-      loading: false,
-      error: '',
-      searchQuery: '',
-    }
+  components: {
+    PlaylistMusicRow,
+    Spinner,
+    SchedulerMusicRow,
+    RequestView
   },
-  methods:{
-    async fetchAllMusic(){
-      this.loading = true
-      this.error = '';
-      try{
-        //BACKEND CONTROLLERBEN /music AZ ÚTVONAL
-        const response = await http.get('api/music');
-        this.songs = response.data.musicJson;
-      }catch(error){
+  data() {
+    return {
+      songs: [],
+      playlistSongs: [],
+      requestedSongs: [],
+      loading: false,
+      error: "",
+      searchQuery: ""
+    };
+  },
+
+  methods: {
+    async fetchAllMusic() {
+      try {
+        const response = await http.get("api/music");
+        this.songs = response.data.musicJson || [];
+      } catch (error) {
         this.error = error.message;
-      }finally{
-        this.loading = false;
       }
     },
-    async fetchSentMusic(){
-      this.loading = true
-      this.error = '';
-      try{
-        const response = await http.get('api/request');
-        this.requestedSongs = response.data.requested_music;
-      }catch(error){
+    async fetchSentMusic() {
+      try {
+        const response = await http.get("api/request");
+        this.requestedSongs = response.data.requested_music || [];
+      } catch (error) {
         this.error = error.message;
-      }finally{
-        this.loading = false;
-      }
-    },async fetchPlaylist(){
-      this.loading = true
-      this.error = '';
-      try{
-        const response = await http.get('api/playlist');
-        this.playlistSongs = response.data.playlist;
-      }catch(error){
-        this.error = error.message;
-      }finally{
-        this.loading = false;
       }
     },
-    addMusicToPlaylist(song){
-      console.log("Hozzáado zene adatai: ", song);
-      const exists = this.playlistSongs.find(item => item.id === song.id)
-      if(!exists){
-        this.playlistSongs.push({...song})//csak akkor adja hozzá ha még nincs benne
-      }else{
+    async fetchPlaylist() {
+      try {
+        const response = await http.get("api/playlist");
+        this.playlistSongs = response.data.playlist || [];
+      } catch (error) {
+        this.error = error.message;
+      }
+    },
+    addMusicToPlaylist(song) {
+      if (!song?.id) return;
+      const exists = this.playlistSongs.some(
+          item => Number(item.id) === Number(song.id)
+      );
+      if (!exists) {
+        this.playlistSongs.push({ ...song });
+      } else {
         alert("Ez a zene már szerepel a listán");
       }
     },
-
     up(id) {
       const index = this.playlistSongs.findIndex(song => song.id === id);
-      if(index > 0){
-        const element = this.playlistSongs.splice(index, 1)[0];
-        this.playlistSongs.splice(index - 1, 0, element);
+      if (index > 0) {
+        const item = this.playlistSongs.splice(index, 1)[0];
+        this.playlistSongs.splice(index - 1, 0, item);
       }
     },
     down(id) {
-      // lejjebb rakni
       const index = this.playlistSongs.findIndex(song => song.id === id);
-      if(index !== -1 && index < this.playlistSongs.length -1){
-        const element = this.playlistSongs.splice(index, 1)[0];
-        this.playlistSongs.splice(index + 1, 0, element);
+      if (index !== -1 && index < this.playlistSongs.length - 1) {
+        const item = this.playlistSongs.splice(index, 1)[0];
+        this.playlistSongs.splice(index + 1, 0, item);
       }
     },
     deleteFromPlaylist(id) {
-      // törölni
       const index = this.playlistSongs.findIndex(song => song.id === id);
-      if(index !== -1){
+      if (index !== -1) {
         this.playlistSongs.splice(index, 1);
       }
+    },
+    isInPlaylist(songId) {
+      return this.playlistSongs.some(
+          item => Number(item.id) === Number(songId)
+      );
     }
   },
-  computed:{
-    filteredSongs(){
-      if(!this.searchQuery){
-        return this.songs;
-      }
-      const query = this.searchQuery.toLowerCase();
 
-      return this.songs.filter(song => song.title.toLowerCase().includes(query) || song.author.toLowerCase().includes(query));
+  computed: {
+    filteredSongs() {
+      const query = this.searchQuery.toLowerCase().trim();
+
+      return this.songs
+          .filter(song => !this.isInPlaylist(song.id))
+          .filter(song => {
+            if (!query) return true;
+
+            return (
+                song.title?.toLowerCase().includes(query) ||
+                song.author?.toLowerCase().includes(query)
+            );
+          });
+    }
   },
-  },
-  mounted(){
-    //itt hívom meg az adatlekérést hogy betöltse a songs tömböt
-    this.fetchAllMusic();
-    //this.loadData();
-    this.fetchSentMusic();
-    this.fetchPlaylist();
-  },
-}
+
+  async mounted() {
+    this.loading = true;
+    await this.fetchAllMusic();
+    await this.fetchSentMusic();
+    //await this.fetchPlaylist();
+    this.loading = false;
+
+    //console.log("songs:", this.songs);
+    //console.log("requested:", this.requestedSongs);
+    //console.log("playlist:", this.playlistSongs);
+  }
+};
 </script>
 
 <template>
   <section class="row px-5 m-2">
-    <!--1. oszlop ami minden zene-->
-    <article class=" col-12 col-md-6 col-lg-4"><!-- itt azt nézze meg hogy be lett e kérve ez a zene-->
-        <h1 class="h2 row">
-          <span class="col-12 col-md-6 col-lg-6">
-            Zenék
-          </span>
-          <span class="query col-12 col-md-6 col-lg-6 d-flex align-items-center">
-            <i class="bi bi-search col-2"></i>
-            <!--Kereső mező-->
-
-            <input v-model="searchQuery"  type="search" placeholder="search" class="fullBorder mw-100 col-10">
-          </span>
-        </h1>
-      <div class="allMusic">
-        <Spinner v-if="loading"/>
-        <SchedulerMusicRow v-for="song in filteredSongs" :key="song.id" :song="song" @add-to-playlist="addMusicToPlaylist"/>
-      </div>
-    </article>
-
-    <!--2. oszlop amibe elágazással hogy bekért zene e vagy nem-->
-    <article class="requestedMusic col-12 col-md-6 col-lg-4">
-      <h1 class="h2 row">
-        <span class="col-12 col-md-6 col-lg-6">Bekért zenék</span>
-      </h1>
-      <div class="requestedMusic">
-      <Spinner v-if="loading"/>
-      <SchedulerMusicRow v-for="song in requestedSongs" :key="song.id" :song="song" @add-to-playlist="addMusicToPlaylist"/>
-      </div>
-    </article>
-
-    <!--3. oszlop megint elágazással, amelyik meg be lett rakva a lejátszási listára-->
+    <!-- 1. Minden zene oszlop -->
     <article class="col-12 col-md-6 col-lg-4">
       <h1 class="h2 row">
-        <span class="col-12 col-md-6 col-lg-6">Lejátszási lista</span>
+        <span class="col-6">Zenék</span>
+        <span class="query col-6 d-flex align-items-center">
+          <i class="bi bi-search col-2"></i>
+          <input
+              v-model="searchQuery"
+              type="search"
+              placeholder="search"
+              class="fullBorder col-10"
+          />
+        </span>
       </h1>
-      <div v-if="playlistSongs.length === 0"><strong>A lejátszási lista üres!</strong></div>
-      <div v-else>
-        <Spinner v-if="loading"/>
-          <PlaylistMusicRow v-for="music in playlistSongs" :key="music.id" :music="music" @up="up" @down="down"
-                            @delete="deleteFromPlaylist"/>
-        </div>
+      <div class="allMusic">
+        <Spinner v-if="loading" />
+        <SchedulerMusicRow
+            v-for="song in filteredSongs"
+            :key="'song-' + song.id"
+            :song="song"
+            :disabled="isInPlaylist(song.id)"
+            @add-to-playlist="addMusicToPlaylist"
+        />
+      </div>
+    </article>
+
+    <!-- 2. Bekért zenék oszlop-->
+    <article class="requestedMusic col-12 col-md-6 col-lg-4">
+      <h1 class="h2">Bekért zenék</h1>
+      <div class="requestedMusic">
+        <Spinner v-if="loading" />
+        <SchedulerMusicRow
+            v-for="song in requestedSongs"
+            :key="'req-' + song.id"
+            :song="song"
+            :disabled="isInPlaylist(song.id)"
+            @add-to-playlist="addMusicToPlaylist"
+        />
+      </div>
+    </article>
+    <!-- 3. Playlist oszlop-->
+    <article class="col-12 col-md-6 col-lg-4">
+      <h1 class="h2">Lejátszási lista</h1>
+      <div v-if="playlistSongs.length === 0">
+        <strong>A lejátszási lista üres!</strong>
+      </div>
+      <div class="playistScroll">
+        <PlaylistMusicRow
+            v-for="music in playlistSongs"
+            :key="'pl-' + music.id"
+            :music="music"
+            @up="up"
+            @down="down"
+            @delete="deleteFromPlaylist"
+        />
+      </div>
     </article>
   </section>
 </template>
@@ -160,8 +185,11 @@ article {
   border: 3px solid black;
   background-color: white;
   height: 85vh;
+  display: flex;
+  flex-direction: column;
 }
-.allMusic, .requestedMusic {
+.allMusic,
+.requestedMusic {
   overflow: scroll;
   height: 75vh;
 }
@@ -175,10 +203,17 @@ article {
 .query {
   border-left: 3px solid black;
 }
-i::before {
-  left: 8vh
-}
 input {
   height: 90%;
+}
+.playistScroll{
+  overflow-y: auto;
+  max-height: 75vh;
+  padding-right: 5px;
+}
+.allMusic,
+.requestedMusic{
+  flex: 1;
+  overflow-y: auto;
 }
 </style>
